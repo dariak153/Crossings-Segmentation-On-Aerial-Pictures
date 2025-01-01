24755fa41e783155096087725324a5066b43efe7
mmcza <marcin4c.mc@gmail.com>
2025-01-02 00:41:10 +0100

    Some fixes and trained custom unet

--- scripts/run_evaluation.py
+++ scripts/run_evaluation.py
@@ -1,13 +1,13 @@
-import sys
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
 import torch
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from segmentation.dataloaders.segmentation_dataloader import SegmentationDataModule
 from segmentation.models.lightning_module import SegmentationLightningModule
 from segmentation.visualization.visualize import visualize_predictions
-
-def evaluate_model(checkpoint, images_dir, masks_dir, batch_size=8, num_workers=2, num_samples=5):
+from segmentation.models.unet import CustomSegmentationModel
+def evaluate_model(model_type, checkpoint, images_dir, masks_dir, batch_size=8, num_workers=2, num_samples=5):
     datamodule = SegmentationDataModule(
         images_dir=images_dir,
         masks_dir=masks_dir,
@@ -20,13 +20,23 @@ def evaluate_model(checkpoint, images_dir, masks_dir, batch_size=8, num_workers=
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = SegmentationLightningModule.load_from_checkpoint(
-        checkpoint,
-        map_location=device
-    )
+
+    if model_type == 'smp_unet':
+        model = SegmentationLightningModule.load_from_checkpoint(
+            checkpoint,
+            map_location=device
+        )
+    elif model_type == 'custom_unet':
+        model = CustomSegmentationModel.load_from_checkpoint(
+            checkpoint,
+            map_location=device
+        )
+    else:
+        raise ValueError(f"Nieznany typ modelu: {model_type}")
+
     model.to(device)
     model.eval()
-    print(f"Model załadowany z {checkpoint}")
+    print(f"Model '{model_type}' załadowany z {checkpoint}")
 
 
     test_loader = datamodule.test_dataloader()
@@ -40,17 +50,24 @@ def evaluate_model(checkpoint, images_dir, masks_dir, batch_size=8, num_workers=
 
     visualize_predictions(images, masks, preds, num_samples=num_samples)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Ewaluacja modelu")
+def main():
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    images_dir: str = os.path.join(parent_dir, 'data', 'data')
+    masks_dir: str = os.path.join(parent_dir, 'data', 'annotated data', 'all_in_one')
+
+    parser = argparse.ArgumentParser(description="Ewaluacja modelu segmentacji")
+    parser.add_argument('--model', type=str, choices=['smp_unet', 'custom_unet'], required=True,
+                        help='Typ modelu do ewaluacji: "smp_unet" lub "custom_unet"')
     parser.add_argument('--checkpoint', type=str, required=True, help='Ścieżka do checkpointu')
-    parser.add_argument('--images_dir', type=str, default='data/data', help='Katalog z obrazami')
-    parser.add_argument('--masks_dir', type=str, default='data/annotated_data/all_in_one', help='Katalog z maskami')
-    parser.add_argument('--batch_size', type=int, default=8)
-    parser.add_argument('--num_workers', type=int, default=2)
+    parser.add_argument('--images_dir', type=str, default=images_dir, help='Katalog z obrazami')
+    parser.add_argument('--masks_dir', type=str, default=masks_dir, help='Katalog z maskami')
+    parser.add_argument('--batch_size', type=int, default=8, help='Rozmiar batcha')
+    parser.add_argument('--num_workers', type=int, default=2, help='Liczba wątków do ładowania danych')
     parser.add_argument('--num_samples', type=int, default=5, help='Liczba próbek do wizualizacji')
     args = parser.parse_args()
 
     evaluate_model(
+        model_type=args.model,
         checkpoint=args.checkpoint,
         images_dir=args.images_dir,
         masks_dir=args.masks_dir,
@@ -59,3 +76,5 @@ if __name__ == "__main__":
         num_samples=args.num_samples
     )
 
+if __name__ == "__main__":
+    main()
--- segmentation/config.py
+++ segmentation/config.py
@@ -1,14 +1,21 @@
 from dataclasses import dataclass
 import torch
+import os
 
 @dataclass
 class DataConfig:
-    images_dir: str = 'data/data'
-    masks_dir: str = 'data/annotated_data/all_in_one'
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    images_dir: str = os.path.join(parent_dir, 'data', 'data')
+    masks_dir: str = os.path.join(parent_dir, 'data', 'annotated data', 'all_in_one')
     batch_size: int = 8
     num_workers: int = 2
     val_split: float = 0.2
     test_split: float = 0.1
+
+    if not os.path.exists(images_dir):
+        raise ValueError(f"Directory {images_dir} does not exist.")
+    if not os.path.exists(masks_dir):
+        raise ValueError(f"Directory {masks_dir} does not exist.")
 
 @dataclass
 class ModelConfig:
@@ -18,9 +25,8 @@ class ModelConfig:
 
 @dataclass
 class TrainerConfig:
-    max_epochs: int = 50
+    max_epochs: int = 70
     accelerator: str = 'gpu' if torch.cuda.is_available() else 'cpu'
     devices: int = 1
     precision: int = 16 if torch.cuda.is_available() else 32
     log_every_n_steps: int = 10
-    early_stopping_patience: int = 10
